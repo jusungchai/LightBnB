b48d434f74088ad67d861158b68b78c39bf9e300
Jay Chai <chunsaa@gmail.com>
2019-11-12 17:40:27 +0000

    changed to show all values

--- 1_queries/5_all_my_reservations.sql
+++ 1_queries/5_all_my_reservations.sql
@@ -1,4 +1,4 @@
-SELECT properties.id, title, cost_per_night, start_date, AVG(property_reviews.rating) as average_rating
+SELECT properties.*, reservations.*, AVG(property_reviews.rating) as average_rating
   FROM properties
   JOIN reservations ON properties.id = reservations.property_id
   JOIN property_reviews ON properties.id = property_reviews.property_id  
