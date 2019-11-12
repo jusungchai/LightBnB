d91bad208a328f125f637ecd463f0884fd07d52f
Jay Chai <chunsaa@gmail.com>
2019-11-12 21:23:14 +0000

    implemented add a property and changed query to LEFT OUTER JOIN because new listings do not have property reviews yet

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -122,7 +122,7 @@ const getAllProperties = function(options, limit = 10) {
   let queryString = `
   SELECT properties.*, avg(property_reviews.rating) as average_rating
   FROM properties
-  JOIN property_reviews ON properties.id = property_id
+  LEFT OUTER JOIN property_reviews ON properties.id = property_id
   `;
 
   if (options.city) {
@@ -162,8 +162,7 @@ const getAllProperties = function(options, limit = 10) {
   }
 
   queryParams.push(limit);  
-  queryString += `LIMIT $${queryParams.length};`; 
-
+  queryString += `LIMIT $${queryParams.length};`;
   return pool.query(queryString, queryParams)
   .then(res => res.rows);
 }
@@ -175,10 +174,20 @@ exports.getAllProperties = getAllProperties;
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
-const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+const addProperty = function(property) {  
+  const queryString = `
+    INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+    RETURNING *;
+  `;
+  const propertyValues = [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms];
+  return pool.query(queryString, propertyValues)
+  .then(res => {
+    if (res.rows) {
+      return res.rows[0];
+    } else {
+      return null;
+    }
+  });
 }
 exports.addProperty = addProperty;
